d070848eb8326d3c3fe24f59c6a1c5462986ddee
KIM INWOO <dlsdn166@gmail.com>
2020-10-06 23:26:47 +0900

    add model

--- store/models.py
+++ store/models.py
@@ -10,7 +10,7 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
-class Product(models.Models):
+class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
@@ -19,4 +19,24 @@ class Product(models.Models):
         return self.name
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer)
+    customer = models.ForeignKey(Customer, on_delete = models.SET_NULL, blank=True, null=True)
+    date_orderd = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete = models.SET_NULL, blank=True, null=True)
+    qunatity = models.IntegerField(default=0, null= True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete = models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete = models.SET_NULL, blank=True, null=True)
+    name = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True)

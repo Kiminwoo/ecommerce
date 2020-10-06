7d7c3696e5f96e20ee622ff99d313c1b6344a459
KIM INWOO <dlsdn166@gmail.com>
2020-10-06 19:30:32 +0900

    add model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Models):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer)
